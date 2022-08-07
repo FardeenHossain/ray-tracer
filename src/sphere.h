700e2d9de4b6c5ec21d97e66d03da48ff4ba7147
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-08-07 14:59:34 +0100

    Added intersect check

--- src/sphere.h
+++ src/sphere.h
@@ -17,4 +17,29 @@ public:
            const float &transp = 0,
            const Vec3f &ec = 0) : center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec), 
            transparency(transp), reflection(refl) {}
+    
+    bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
+    {
+        Vec3f l = center - rayorig;
+        float tca = l.dot(raydir);
+
+        if (tca < 0) 
+        {
+            return false;
+        }
+
+        float d2 = l.dot(l) - tca * tca;
+
+        if (d2 > radius2) 
+        {
+            return false;
+        }
+
+        float thc = sqrt(radius2 - d2);
+        
+        t0 = tca - thc;
+        t1 = tca + thc;
+        
+        return true;
+    }
 };
