77f9eedb90399b163786aefe843087a88e0553fb
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-08-07 15:01:11 +0100

    Added ray intersection

--- src/trace.h
+++ src/trace.h
@@ -7,3 +7,35 @@ float mix(const float &a, const float &b, const float &mix)
 {
     return b * mix + a * (1 - mix);
 }
+
+Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir, const std::vector<Sphere> &spheres, const int &depth)
+{
+    float tnear = INFINITY;
+    const Sphere* sphere = NULL;
+
+    // Find intersection of this ray with the sphere in the scene
+    for (unsigned i = 0; i < spheres.size(); ++i) 
+    {
+        float t0 = INFINITY, t1 = INFINITY;
+
+        if (spheres[i].intersect(rayorig, raydir, t0, t1)) 
+        {
+            if (t0 < 0) 
+            {
+                t0 = t1;
+            }
+
+            if (t0 < tnear) 
+            {
+                tnear = t0;
+                sphere = &spheres[i];
+            }
+        }
+    }
+
+    // If there's no intersection return black or background color
+    if (!sphere) 
+    {
+        return Vec3f(2);
+    }
+}
